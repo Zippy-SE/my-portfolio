40d35ac4bf9cbb0693548e24cd9afbed1686bdd2
Zippy-SE <bdb1204@yahoo.com>
2025-10-13 03:59:02 -0400

    Addition of input_output C program

---
+++ input_output.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+main ()
+{
+    char myboss[15];
+    printf ("What is the name of your boss.\n");
+    scanf ("%s", myboss);
+    printf ("%s", myboss);
+    printf ("? That sounds like the name of a moron!");
+}
