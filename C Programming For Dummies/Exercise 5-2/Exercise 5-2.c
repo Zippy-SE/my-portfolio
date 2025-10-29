aa526a16113d6ebde155d66e9d3f06ef0d31efa1
Zippy-SE <bdb1204@yahoo.com>
2025-10-29 04:06:37 -0400

    New Exercise 5-2 version

--- C Programming For Dummies/Exercise 5-2/Exercise 5-2.c
+++ C Programming For Dummies/Exercise 5-2/Exercise 5-2.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
     
     printf("The number %d is an integer.\n",127);
-    printf("The number %f is a float.\n",3.1415926535);
+    printf("The number %1.2f is a float.\n",3.1415926535);
     printf("The number %d is an integer.\n",122013);
-    printf("The number %f is a float.\n",0.00008);
-    
-    
+    printf("The number %1.1f is a float.\n",0.00008);
+
+    // Pause so the console stays open when launched from VS Code or an external terminal
+    printf("\nPress Enter to exit...");
+    fflush(stdout);
+    getchar();
+
     return (0);
 }
