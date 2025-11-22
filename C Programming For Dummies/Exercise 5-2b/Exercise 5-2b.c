1ea4e76b329842017a54e377834f12eb71575ff3
Zippy-SE <bdb1204@yahoo.com>
2025-11-22 13:51:34 -0500

    Exercise 5.2b added

---
+++ C Programming For Dummies/Exercise 5-2b/Exercise 5-2b.c
@@ -0,0 +1,13 @@
+    #include <stdio.h>
+    #include <stdlib.h>
+
+    int main(void) {
+        puts("Values 8 and 2:");
+        printf("Addition is %d\n", 8+2);
+        printf("Subtraction is %d\n", 8-2);
+        printf("Multiplication is %d\n", 8*2);
+        printf("Division is %d\n", 8/2);
+        system("pause");
+        return (0);
+    }
+    
--- C# Programs/helloworld/obj/Debug/net9.0/helloworld.AssemblyInfo.cs
+++ C# Programs/helloworld/obj/Debug/net9.0/helloworld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+094837805974c6847714deaee392ae03c628c65d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ef4875bff3d99668085f226444290264c2aca09")]
 [assembly: System.Reflection.AssemblyProductAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
