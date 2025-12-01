e36fe1335adb7728305a4df3f68da9ca7288d628
Zippy-SE <bdb1204@yahoo.com>
2025-11-30 22:57:40 -0500

    New program added: initials.cpp

---
+++ C++ Programs/initials.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+  std::cout << "BBBB     BBBB\n";
+  std::cout << "B   B    B   B\n";
+  std::cout << "B   B    B   B\n";
+  std::cout << "BBBB     BBBB\n";
+  std::cout << "B   B    B   B\n";
+  std::cout << "B   B    B   B\n";
+  std::cout << "BBBB     BBBB\n"; 
+ 
+ 
+  return 0;
+}
