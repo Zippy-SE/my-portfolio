15bcda57aaccfada716d9ce835884d90e522d338
Zippy-SE <bdb1204@yahoo.com>
2026-02-15 16:57:59 -0500

    Exercises 6-1 & 6-2 added

--- Python Programs/Intro to Python Coding by Code Platoon/Guessing the Word.py
+++ Python Programs/Intro to Python Coding by Code Platoon/Guessing the Word.py
@@ -1,9 +1,16 @@
 secret_word = "python"
 guess = ""
+attempts = 0
+max_attempts = 3
 
 print("Guess the secret word!")
 
-while guess != secret_word:
+while guess != secret_word and attempts < max_attempts:
     guess = input("your guess: ")
-
-print("Congratulations! You guessed the secret word.")
+    attempts += 1
+    if guess != secret_word:
+        print("Wrong guess. Try again!")    
+if guess == secret_word:
+    print("Congratulations! You guessed the secret word.")
+else:
+    print("Sorry, you've used all your attempts. Better luck next time!")   
